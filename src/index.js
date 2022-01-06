2d70de4b22bb697b0b8ff043de4ec0042eb150cb
icy-purple <90755919+icy-purple@users.noreply.github.com>
2022-01-07 02:16:05 +0300

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (Number.isInteger(n)) {
+        if (n<0) {
+            n *= -1;
+        }
+        let a = n.toString().split('').reverse().join('');
+        return parseInt(a);
+    }
 }
